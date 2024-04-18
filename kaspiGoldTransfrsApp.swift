3d0fb3cbd6bdde62af0df071f58d0dcbe3c6f3af
NuraliShamel2008 <162154291+NuraliShamel@users.noreply.github.com>
2024-04-18 21:44:55 +0500

    Add files via upload

---
+++ kaspiGoldTransfrsApp.swift
@@ -0,0 +1,17 @@
+//
+//  kaspiGoldTransfrsApp.swift
+//  kaspiGoldTransfrs
+//
+//  Created by Nurali Shamel on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct kaspiGoldTransfrsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
